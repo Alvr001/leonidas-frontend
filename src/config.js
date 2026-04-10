ce86d72c57d0f0545c3f6ab4be56648930f14199
Alvr001 <alvaro.caldas12@gmail.com>
2026-04-10 13:30:05 -0500

    Update URLs to Render backend

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://192.168.100.45:8080';
-const WS_URL = 'ws://192.168.100.45:8080/ws-native';
+const API_URL = 'https://alpheratz-backend.onrender.com';
+const WS_URL = 'wss://alpheratz-backend.onrender.com/ws-native';
 
 export { API_URL, WS_URL };
